e0593d91b015dc684a7025556467c689358502fd
lphillipe <luisphillipe200@gmail.com>
2026-02-03 07:28:20 -0300

    add schemas and configure users schemas

---
+++ fast_master/car_api/car_api/schemas/users.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, EmailStr
+
+
+class UserSchema(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+
+
+class UserPublicSchema(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
