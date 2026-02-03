906d322bede5693d7fbc6d3b1fff9c6e1dc5c006
lphillipe <luisphillipe200@gmail.com>
2026-02-03 07:13:38 -0300

    indentation users.py

--- fast_master/car_api/car_api/routers/users.py
+++ fast_master/car_api/car_api/routers/users.py
@@ -6,17 +6,19 @@ router = APIRouter()
 @router.get('/', status_code=status.HTTP_200_OK)
 async def list_users():
     return {
-        'users': {
-        {    'id': 1,
-            'email': 'joao@gmail.com',
-        },
-        {
-            'id': 2,
-            'email': 'maria@gmail.com',
-        },
-        {
-            'id': 3,
-            'email': 'luis@gmail.com',
-        },
-        }
+        'users': [
+            {
+                'id': 1,
+                'email': 'pycodebr@gmail.com',
+            },
+            {
+                'id': 2,
+                'email': 'joao@gmail.com',
+            },
+            {
+                'id': 3,
+                'email': 'mario@gmail.com',
+            },
+        ]
     }
+
