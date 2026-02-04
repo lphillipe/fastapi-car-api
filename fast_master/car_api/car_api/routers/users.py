4da8bcb6a4b590423eb56cd11f58dc6e91252423
lphillipe <luisphillipe200@gmail.com>
2026-02-04 06:45:54 -0300

    create router delete in users

--- fast_master/car_api/car_api/routers/users.py
+++ fast_master/car_api/car_api/routers/users.py
@@ -22,19 +22,28 @@ async def create_user(user: UserSchema):
 
 
 @router.get(
-        path= '/', 
-        status_code=status.HTTP_200_OK, 
-        response_model=UserListPublicSchema,
-    )
+    path= '/', 
+    status_code=status.HTTP_200_OK, 
+    response_model=UserListPublicSchema,
+)
 async def list_users():
     return { 'users': USERS }
 
 @router.put(
-        path='/{user_id}',
-        status_code=status.HTTP_201_CREATED,
-        response_model=UserPublicSchema,
+    path='/{user_id}',
+    status_code=status.HTTP_201_CREATED,
+    response_model=UserPublicSchema,
 )
 async def update_user(user_id: int, user: UserSchema):
     user_with_id = UserPublicSchema(**user.model_dump(), id=user_id)
     USERS[user_id - 1] = user_with_id
     return user_with_id
+
+
+@router.delete(
+    path='/{user_id}',
+    status_code=status.HTTP_204_NO_CONTENT,
+)
+async def delete_user(user_id: int):
+    del USERS[user_id - 1]
+    return
