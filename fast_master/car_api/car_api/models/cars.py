1dd30a85081ada27953b86eb2703fa09c96af372
lphillipe <luisphillipe200@gmail.com>
2026-02-05 07:00:12 -0300

    create models.cars and config

---
+++ fast_master/car_api/car_api/models/cars.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from typing import Optional
+
+from sqlalchemy import func 
+from sqlalchemy import ForeignKey, String, Text, Numeric, Integer
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from car_api.models import Base
+
+
+class Brand(Base):
+    __tablename__= 'brands'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String[50], unique=True)
+    is_active: Mapped[bool] = mapped_column(default=True)
+    description: Mapped[Optional[str]] = mapped_column(Text, default=None)
+    
+    created_at: Mapped[str] = mapped_column(server_default=func.now())
+    update_at: Mapped[str] = mapped_column(
+        onupdate=func.now(), server_default=func.now(),
+    )
+    
