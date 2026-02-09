7141717db5b995554681bf1a8a02e31d093d4c5d
lphillipe <luisphillipe200@gmail.com>
2026-02-09 06:15:01 -0300

    create routers brand

---
+++ fast_master/car_api/car_api/routers/brands.py
@@ -0,0 +1,34 @@
+from fastapi import APIRouter,status, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from car_api.core.database import get_session
+from car_api.models.cars import Brand
+from car_api.schemas.brands import (
+    BrandSchema,
+    BrandPublicSchema,
+)
+
+router = APIRouter()
+
+@router.post(
+    path='/',
+    status_code=status.HTTP_201_CREATED,
+    response_model=BrandPublicSchema,
+    summary='Criar nova marca',
+)
+
+async def create_brand(
+    brand: BrandSchema,
+    db: AsyncSession = Depends(get_session),
+):
+    db_brand = Brand(
+        name=brand.name,
+        description=brand.description,
+        is_active=brand.is_active,
+    )
+
+    db.add(db_brand)
+    await db.commit()
+    await db.refresh(db_brand)
+
+    return db_brand
