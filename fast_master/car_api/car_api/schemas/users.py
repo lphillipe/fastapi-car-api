ea1dd620d9bec88c4ab23f5981acd6b0a4ae4df6
lphillipe <luisphillipe200@gmail.com>
2026-02-09 05:44:14 -0300

    add field_validator in schemas

--- fast_master/car_api/car_api/schemas/users.py
+++ fast_master/car_api/car_api/schemas/users.py
@@ -1,13 +1,25 @@
 from typing import Optional, List
 
 from datetime import datetime
-from pydantic import BaseModel, ConfigDict, EmailStr
+from pydantic import BaseModel, ConfigDict, EmailStr, field_validator
 
 
 class UserSchema(BaseModel):
     username: str
     email: EmailStr
     password: str
+
+    @field_validator('username')
+    def username_min_length(cls, v):
+        if len(v) <3:
+            raise ValueError('Username deve ter pelo menos 3 caracteres')
+        return v
+    
+    @field_validator('password')
+    def password_min_lenght(cls, v):
+        if len(v) < 6:
+            raise ValueError('Senha deve ter pelo menos 6 caracteres')
+        return v
 
 
 class UserUpdateSchema(BaseModel):
