1bfc9f990013a503630725c5605a799d26504e53
lphillipe <luisphillipe200@gmail.com>
2026-02-10 06:56:20 -0300

    include cars in app

--- fast_master/car_api/car_api/app.py
+++ fast_master/car_api/car_api/app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, status
 
-from car_api.routers import users, brands
+from car_api.routers import users, brands, cars
 
 
 
@@ -16,6 +16,13 @@ app.include_router(
     router=brands.router,
     prefix='/api/v1/brands',
     tags=['brands'],
+)
+
+
+app.include_router(
+    router=cars.router,
+    prefix='/api/v1/cars',
+    tags=['cars'],
 )
 
 @app.get('/health_check', status_code=status.HTTP_200_OK)
