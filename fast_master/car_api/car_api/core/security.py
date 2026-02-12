4e53ea7743985e26309293906eb025639d8d16a6
lphillipe <luisphillipe200@gmail.com>
2026-02-12 06:47:07 -0300

    configure core.settings create acess token

--- fast_master/car_api/car_api/core/security.py
+++ fast_master/car_api/car_api/core/security.py
@@ -1,9 +1,31 @@
+from datetime import datetime, timedelta, timezone
+from typing import Dict, Optional
+
+import jwt
 from pwdlib import PasswordHash
 
+
+from car_api.core.settings import Settings
+
 pwd_context = PasswordHash.recommended()
+settings = Settings()
 
 def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(data: Dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(
+        minutes=settings.JWT_EXPIRATION_MINUTES
+    )
+    to_encode.update({'exp': expire})
+
+    encoded_jwt = jwt.encode(
+        to_encode, settings.JWT_SECRET_KEY, algorithm=settings.JWT_ALGORITHM
+    )
+
+    return encoded_jwt
