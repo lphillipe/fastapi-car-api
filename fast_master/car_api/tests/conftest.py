f6594001d675fc59a539d8845d2aaceb815eb683
lphillipe <luisphillipe200@gmail.com>
2026-02-16 10:59:45 -0300

    test config: conftest

--- fast_master/car_api/tests/conftest.py
+++ fast_master/car_api/tests/conftest.py
@@ -1,7 +1,11 @@
+import pytest
 import pytest_asyncio
 
+from fastapi.testclient import TestClient
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
+from car_api.app import app
+from car_api.core.database import get_session
 from car_api.models import Base
 
 
@@ -19,3 +23,15 @@ async def session():
 
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
+
+
+@pytest.fixture
+def client(session):
+    def get_session_override():
+        return session
+
+    with TestClient(app) as client:
+        app.dependency_overrides[get_session] = get_session_override
+        yield client
+
+    app.dependency_overrides.clear() 
